a145a3a11293f5dc3c94b060f0152b42e90adc48
Ansgar Mertens <a@amrtns.com>
2017-10-30 15:58:54 +0100

    inherit point from other component. add style to select

--- src/interaction/select.js
+++ src/interaction/select.js
@@ -2,11 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ol from 'openlayers';
 import OLInteraction from './ol-interaction';
+import { buildStyle } from '../style';
 
 export default class Select extends OLInteraction {
   createInteraction (props) {
     return new ol.interaction.Select({
-      condition: props.condition
+      condition: props.condition,
+      style: props.style ? buildStyle(props.style) : undefined
     })
   }
 }
