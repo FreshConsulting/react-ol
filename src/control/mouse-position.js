aae56985782230bad187b823c9e3449fb851776f
Jason Tan <jason.tan@freshconsulting.com>
2019-08-11 16:58:04 -0700

    Upgrade control/MousePosition

--- src/control/mouse-position.js
+++ src/control/mouse-position.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import ol from 'openlayers';
+import OLMousePosition from 'ol/control/MousePosition';
+import Projection from 'ol/proj/Projection';
 import OLControl from './ol-control';
 
 export default class MousePosition extends OLControl {
   createControl (props) {
-    return new ol.control.MousePosition({
+    return new OLMousePosition({
       className: props.className,
       coordinateFormat: props.coordinateFormat,
       projection: props.projection,
@@ -18,7 +19,7 @@ MousePosition.propTypes = Object.assign({}, OLControl.propTypes, {
   className: PropTypes.string,
   coordinateFormat: PropTypes.func,
   projection: PropTypes.oneOfType([
-    PropTypes.instanceOf(ol.proj.Projection),
+    PropTypes.instanceOf(Projection),
     PropTypes.string
   ]),
   undefinedHTML: PropTypes.string
