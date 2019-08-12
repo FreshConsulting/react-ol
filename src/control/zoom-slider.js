1d5d5f6653804b9801f1ffd331a4441b4d0e0947
Jason Tan <jason.tan@freshconsulting.com>
2019-08-11 17:02:14 -0700

    Upgrade control/ZoomSlider

--- src/control/zoom-slider.js
+++ src/control/zoom-slider.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import ol from 'openlayers';
+import OLZoomSlider from 'ol/control/ZoomSlider';
 import OLControl from './ol-control';
 
 export default class ZoomSlider extends OLControl {
   createControl (props) {
-    return new ol.control.ZoomSlider({
+    return new OLZoomSlider({
       className: props.className,
       duration: props.duration,
       maxResolution: props.maxResolution,
