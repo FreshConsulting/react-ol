97c34f72ce730d01728b0a0614d10a9b0e610361
Jason Tan <jason.tan@freshconsulting.com>
2019-08-13 13:26:26 -0700

    Convert from babel to webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'react-ol',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react'],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
+      }
+    ]
+  }
+}
